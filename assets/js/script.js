704fcb89584c90b1243af80291dbd0bd03f854e8
Karthik041297 <karthik042512@gmail.com>
2025-01-30 16:19:02 +0530

    Initial commit for SPARE-PARTS-DYNAMIC-WEBSITE

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+
+
+ /* MOBILE NAVBAR TOGGLE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+navToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+/*  javascript for scrolldown  */
+
+
+const navbarLinks = document.querySelectorAll('.navbar-link');
+
+navbarLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
